41835c923da07b3a0b8e8f76dcd8f6df6f2c14f1
fatima461 <fatimatuzzehraarshad2020@gmail.com>
2024-11-12 01:50:01 -0800

    Add files via upload

--- todolist.js
+++ todolist.js
@@ -4,5 +4,17 @@ console.log(Array.isArray(myArray))
 
 console.log(myArray.length);
 
-myArray.splice(0,2)
-console.log(myArray);
+//myArray.splice(0,2)
+//console.log(myArray);
+
+myArray.push('purple');
+
+
+console.log(myArray[2]);
+
+let mixedBag = ['book',5,true,['nested','array']]
+console.log(mixedBag);
+
+for (let i=0; i <mixedBag.length; i++){
+    console.log(mixedBag[i]);
+}
---
+++ todolist1.js
@@ -0,0 +1,5 @@
+const todoList=[];
+function addToDo()
+{
+    let inputText=document.querySelector('.js-')
+}
